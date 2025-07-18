f559cfb0fbd3a9f4bd579928f9fee098f595a6f4
Xuan25 <xuan@xuan25.com>
2025-07-18 01:19:10 +0100

    RayCastHelper: Auto adjust collider size for scrollviews

--- RayCastHelper/Runtime/RayCastScrollViewHelper.cs
+++ RayCastHelper/Runtime/RayCastScrollViewHelper.cs
@@ -1,7 +1,9 @@
 ﻿
+using System;
 using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using VRC.SDKBase;
 using VRC.Udon;
 
@@ -20,15 +22,30 @@ namespace RayCastUtils {
 
         public float joystickScrollScalar = 10f;
 
+        public bool horizontal = false;
+
+        private RectTransform scrollRectTransform;
+
         private float currentRightVerticalValue = 0;
 
         void Start()
         {
-            
+            scrollRectTransform = scrollRect.GetComponent<RectTransform>();
         }
 
-        void Update()
+        public void FixedUpdate()
         {
+            // Adjust the size of the collider to match the scroll view
+            
+            if (rayCastHelper.targetCollider.GetType() == typeof(BoxCollider))
+            {
+                BoxCollider boxCollider = (BoxCollider)rayCastHelper.targetCollider;
+                boxCollider.size = scrollRectTransform.rect.size;
+                boxCollider.center = scrollRectTransform.rect.center;
+            }
+
+            // Check for mouse scroll input and right hand joystick input
+
             float mouseScrollDelta = Input.GetAxis("Mouse ScrollWheel");
 
             if (mouseScrollDelta != 0)
@@ -53,13 +70,26 @@ namespace RayCastUtils {
 
         private void Scroll(float value)
         {
-            float contentHeight = scrollRect.content.rect.height;
-            float currentAbsPosition = scrollRect.verticalNormalizedPosition * contentHeight;
-            float newAbsPosition = currentAbsPosition + (value * scrollSensitivity);
-            float normalizedPosition = Mathf.Clamp01(newAbsPosition / contentHeight);
-            scrollRect.verticalNormalizedPosition = normalizedPosition;
+            if (!horizontal)
+            {
+                float contentHeight = scrollRect.content.rect.height;
+                float currentAbsPosition = scrollRect.verticalNormalizedPosition * contentHeight;
+                float newAbsPosition = currentAbsPosition + (value * scrollSensitivity);
+                float normalizedPosition = Mathf.Clamp01(newAbsPosition / contentHeight);
+                scrollRect.verticalNormalizedPosition = normalizedPosition;
 
-            Debug.Log($"Scroll Value: {value}, Current Position: {currentAbsPosition}, New Position: {newAbsPosition}, Normalized Position: {normalizedPosition}");
+                Debug.Log($"Vertical Scroll Value: {value}, Current Position: {currentAbsPosition}, New Position: {newAbsPosition}, Normalized Position: {normalizedPosition}");
+            }
+            else
+            {
+                float contentWidth = scrollRect.content.rect.width;
+                float currentAbsPosition = scrollRect.horizontalNormalizedPosition * contentWidth;
+                float newAbsPosition = currentAbsPosition + (-value * scrollSensitivity);
+                float normalizedPosition = Mathf.Clamp01(newAbsPosition / contentWidth);
+                scrollRect.horizontalNormalizedPosition = normalizedPosition;
+
+                Debug.Log($"Horizontal Scroll Value: {value}, Current Position: {currentAbsPosition}, New Position: {newAbsPosition}, Normalized Position: {normalizedPosition}");
+            }
         }
 
         public override void InputLookVertical(float value, VRC.Udon.Common.UdonInputEventArgs args)
