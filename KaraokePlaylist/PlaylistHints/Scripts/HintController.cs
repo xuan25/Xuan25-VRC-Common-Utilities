53dc85fb22b219b83a4ca0a8f78bf0753d2dbea5
Xuan25 <xuan@xuan25.com>
2025-08-26 19:22:21 +0100

    Add missing files for KaraokePlaylist

---
+++ KaraokePlaylist/PlaylistHints/Scripts/HintController.cs
@@ -0,0 +1,87 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Persistence;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Hints
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class HintController : UdonSharpBehaviour
+    {
+        public string hintKey = "DefaultHint";
+
+        public bool showInPCVR = true;
+        public bool showInPCDesktop = true;
+        public bool showInAndroidVR = true;
+        public bool showInAndroidMobile = true;
+        public bool showInIOS = true;
+        public bool showInVisionOS = true;
+
+        void Start()
+        {
+#if UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX
+            if (Networking.LocalPlayer.IsUserInVR()) {
+                if(!showInPCVR)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                if(!showInPCDesktop)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+#elif UNITY_ANDROID
+            if (Networking.LocalPlayer.IsUserInVR()) {
+                if(!showInAndroidVR)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                if(!showInAndroidMobile)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+#elif UNITY_IOS || UNITY_VISIONOS
+            if (Networking.LocalPlayer.IsUserInVR()) {
+                if(!showInVisionOS)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                if(!showInIOS)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+#else
+            gameObject.SetActive(false);
+#endif
+        }
+
+        public override void OnPlayerDataUpdated(VRCPlayerApi player, PlayerData.Info[] infos)
+        {
+            if (player.isLocal)
+            {
+                if (PlayerData.HasKey(player, hintKey) && PlayerData.GetBool(player, hintKey)) {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+
+        public override void Interact()
+        {
+            gameObject.SetActive(false);
+            PlayerData.SetBool(hintKey, true);
+        }
+    }
+}
