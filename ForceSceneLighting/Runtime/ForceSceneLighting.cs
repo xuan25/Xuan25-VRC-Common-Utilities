dc8e45c4fa362556bbc6db9634528b6220d8cfd3
Xuan25 <xuan@xuan25.com>
2025-09-12 05:41:25 +0100

    Add ForceSceneLighting for liltoon and Poiyomi shaders

---
+++ ForceSceneLighting/Runtime/ForceSceneLighting.cs
@@ -0,0 +1,30 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace ForceSceneLighting
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class ForceSceneLighting : UdonSharpBehaviour
+    {
+        private int idUdonForceSceneLighting = -1;
+
+        private bool isInitialized = false;
+
+        public void OnEnable()
+        {
+            if (!isInitialized)
+            {
+                idUdonForceSceneLighting = VRCShader.PropertyToID("_UdonForceSceneLighting");
+                isInitialized = true;
+            }
+            VRCShader.SetGlobalFloat(idUdonForceSceneLighting, 1);
+        }
+
+        public void OnDisable()
+        {
+            VRCShader.SetGlobalFloat(idUdonForceSceneLighting, 0);
+        }
+    }
+}
