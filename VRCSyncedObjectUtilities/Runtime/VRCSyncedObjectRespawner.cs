82027547bb3b3294877dcfbb7275f38b5b280c68
Xuan25 <xuan@xuan25.com>
2026-02-27 18:23:27 +0000

    feat (VRCSyncedObjectUtilities): add VRCSyncedObjectRespawner and related assets

---
+++ VRCSyncedObjectUtilities/Runtime/VRCSyncedObjectRespawner.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Xuan25.VRCSyncedObjectUtilities
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class VRCSyncedObjectRespawner : UdonSharpBehaviour
+    {
+        public VRCObjectSync[] vRCObjectSyncs;
+
+        public override void Interact()
+        {
+            foreach (VRCObjectSync vrcObjectSync in vRCObjectSyncs)
+            {
+                if (vrcObjectSync != null)
+                {
+                    Networking.SetOwner(Networking.LocalPlayer, vrcObjectSync.gameObject);
+                    vrcObjectSync.Respawn();
+                }
+            }
+        }
+    }
+}
