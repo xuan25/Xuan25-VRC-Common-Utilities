51476856ec99a76144ac6e59f251d7126dc2359d
Xuan25 <xuan@xuan25.com>
2026-04-25 22:15:49 +0100

    feat(AnimatorUtilities): add trigger-based control

---
+++ AnimatorUtilities/Runtime/DiscreteAnimatorDriverIndexerTrigger.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Xuan25.AnimatorUtilities
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class DiscreteAnimatorDriverIndexerTrigger : DiscreteAnimatorDriverIndexer
+    {
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            SetParameter();
+        }
+    }
+}
