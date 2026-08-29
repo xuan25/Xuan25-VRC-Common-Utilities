0f876a43541d52bb25b066511d06db208dc477ed
Xuan25 <xuan@xuan25.com>
2026-08-29 01:30:38 +0100

    feat(SceneAssembly): add ReParentPart component for dynamic re-parenting of GameObjects during build

--- SceneAssembly/Editor/SceneAssemblyHelper.cs
+++ SceneAssembly/Editor/SceneAssemblyHelper.cs
@@ -14,6 +14,7 @@ namespace SceneAssembly
 
         public void OnProcessScene(UnityEngine.SceneManagement.Scene scene, BuildReport report)
         {
+            ProcessReparentParts();
             ProcessSceneParts();
             ProcessStaticBatchingGroups();
         }
@@ -50,6 +51,76 @@ namespace SceneAssembly
                 scenePart.transform.localRotation = Quaternion.identity;
                 scenePart.transform.localScale = Vector3.one;
                 scenePart.gameObject.SetActive(true);
+            }
+        }
+
+        private void ProcessReparentParts()
+        {
+            ReParentPart[] targets = Object.FindObjectsOfType<ReParentPart>(true);
+            if (targets == null || targets.Length == 0)
+            {
+                return;
+            }
+
+            for (int i = 0; i < targets.Length; i++)
+            {
+                ReParentPart target = targets[i];
+                string targetParentName = target.targetParentName;
+                bool worldPositionStays = target.worldPositionStays;
+                Transform targetTransform = target.transform;
+
+                // Remove the marker ReParentPart component
+
+                Object.DestroyImmediate(target, false);
+
+                // Resolve the target parent GameObject
+
+                Transform targetParentTransform;
+
+                if (targetParentName.StartsWith("."))
+                {
+                    // Relative path
+                    string currentPatentFullPath = "";
+                    for (Transform t = targetTransform.parent; t != null; t = t.parent)
+                    {
+                        currentPatentFullPath = "/" + t.name + currentPatentFullPath;
+                    }
+
+                    string relativePath = currentPatentFullPath + "/" + targetParentName;
+
+                    string absolutePath = System.IO.Path.GetFullPath(relativePath);
+
+                    // resolve relative path to absolute path
+                    targetParentTransform = targetTransform.root.Find(relativePath);
+                    if (targetParentTransform == null)
+                    {
+                        Debug.LogError($"[{nameof(SceneAssemblyHelper)}] Could not find target parent '{targetParentName}' for GameObject '{target.gameObject.name}' using relative path, which resolved to '{absolutePath}'.");
+                        continue;
+                    }
+                }
+                else if (targetParentName.StartsWith("/"))
+                {
+                    // Absolute path
+                    targetParentTransform = targetTransform.root.Find(targetParentName);
+                    if (targetParentTransform == null)
+                    {
+                        Debug.LogError($"[{nameof(SceneAssemblyHelper)}] Could not find target parent '{targetParentName}' for GameObject '{target.gameObject.name}' using absolute path.");
+                        continue;
+                    }
+                }
+                else
+                {
+                    // Name only, search globally
+                    targetParentTransform = GameObject.Find(targetParentName)?.transform;
+                    if (targetParentTransform == null)
+                    {
+                        Debug.LogError($"[{nameof(SceneAssemblyHelper)}] Could not find target parent '{targetParentName}' for GameObject '{target.gameObject.name}' using name only.");
+                        continue;
+                    }
+                }
+
+                // Re-parent the target GameObject to the found parent
+                targetTransform.SetParent(targetParentTransform, worldPositionStays);
             }
         }
 
---
+++ SceneAssembly/Runtime/ReParentPart.cs
@@ -0,0 +1,51 @@
+﻿
+using UdonSharp;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace SceneAssembly
+{
+    public class ReParentPart : MonoBehaviour
+    {
+        [Tooltip("The name of the parent GameObject to reparent this GameObject to.")]
+        public string targetParentName = "";
+
+        [Tooltip("Whether to keep the world position, rotation, and scale of the GameObject when reparenting.")]
+        public bool worldPositionStays = false;
+    }
+
+#if UNITY_EDITOR
+
+    [CustomEditor(typeof(ReParentPart))]
+    public class ReparentPartEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            ReParentPart scenePart = (ReParentPart)target;
+
+            // Draw default inspector
+            DrawDefaultInspector();
+
+            EditorGUILayout.HelpBox("This component is used to reparent a GameObject to a specified parent during build time. ", MessageType.Info);
+            EditorGUILayout.HelpBox("Parent name can be specified with a Game Object's name, relative path, or absolute path.", MessageType.Info);
+            EditorGUILayout.HelpBox("Relative paths begin with a dot (.) and are relative to the current GameObject.", MessageType.Info);
+            EditorGUILayout.HelpBox("Absolute paths begin with a slash (/) and are relative to the root of the scene hierarchy.", MessageType.Info);
+
+            // check static batching flag
+            // if (GameObjectUtility.AreStaticEditorFlagsSet(scenePart.gameObject, StaticEditorFlags.BatchingStatic) &&
+            //     scenePart.GetComponent<StaticBatchingGroup>() == null)
+            // {
+            //     EditorGUILayout.HelpBox("Static Batching is currently enabled for this Scene Part. To minimize potential issues with automatic batching in the editor, it is recommended to use the StaticBatchingGroup component instead, which allows explicit combination of child objects.", MessageType.Warning);
+            //     if (GUILayout.Button("Add StaticBatchingGroup"))
+            //     {
+            //         Undo.AddComponent<StaticBatchingGroup>(scenePart.gameObject);
+            //     }
+            // }
+        }
+    }
+
+#endif
+
+}
